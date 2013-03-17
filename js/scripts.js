e734116d58e4533f970c712e9858595c4a1274ca
Brian <brianjoshuanoah@gmail.com>
2013-03-17 03:10:23 -0700

    Stuff is working!

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var api_token = '353ddbae79fe8b53ddb36fbe5f389e68';
 
 $.ajax({
 	type: 'GET',
-	url: 'http://imac.local:3000/projects',
+	url: 'http://gentle-forest-6759.herokuapp.com/projects',
 	beforeSend: function(xhr){
 		xhr.setRequestHeader('api_token', api_token);
 	},
@@ -23,7 +23,7 @@ $.ajax({
 
 $.ajax({
 	type: 'GET',
-	url: 'http://imac.local:3000/members',
+	url: 'http://gentle-forest-6759.herokuapp.com/members',
 	beforeSend: function(xhr){
 		xhr.setRequestHeader('api_token', api_token);
 	},
@@ -35,60 +35,109 @@ $.ajax({
 		console.log(e.status);
 	},
 	success: function (s) {
-		console.log('success', s);
+                        var members = s.membership;
+                        var member_count = members.length;
+                        for (i = 0; i < member_count; i++) {
+                            var member = members[i];
+                            member.unique_id = member.person.name.replace(" ", '');
+                            if (member.role == 'Owner' || member.role == 'Member') {
+                                $('#user_columns').append('<div class="user_column" id="' + member.unique_id + '"><header>' + member.person.name + '</header><div class="user_stories"></div></div>');
+                            }
+                        }
+                        $.ajax({
+                            type: 'GET',
+                            url: 'http://gentle-forest-6759.herokuapp.com/current_iterations',
+                            beforeSend: function(xhr){
+                                xhr.setRequestHeader('api_token', api_token);
+                            },
+                            error: function (e) {
+                                console.log('error', e);
+                                console.log(e.responseText);
+                                console.log(e.statusText);
+                                console.log(e.status);
+                            },
+                            success: function (s) {
+                                console.log('success', s);
+                                var iterations = s.iteration;
+                                var iteration_count = iterations.length;
+                                for (i=0; i < iteration_count; i++) {
+                                    var iteration = iterations[i];
+                                    var stories = iteration.stories.story;
+                                    if (stories !== undefined) {
+                                        console.log(i+'*');
+                                        var story_count = stories.length;
+                                        console.log(story_count);
+                                        if (story_count !== undefined) {
+                                            for (var count=0; count < story_count; count++) {
+                                                var story = stories[count];
+                                                if (story.story_type !== 'release' && story.owned_by !== undefined) {
+                                                    var member = story.owned_by.replace(' ', ''),
+                                                        story_name = story.name,
+                                                        difficulty = story.estimate,
+                                                        status = story.current_state,
+                                                        id = story.id;
+                                                    $('#' + member).addClass('active');
+                                                    $('#' + member + ' .user_stories').append('<div id="' + id + '" class="user_story" difficulty="' + difficulty+ '" status="' + status+ '"><span>' + story_name + '</span></div>');
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        });
 	}
 });
 
-$.ajax({
-	type: 'GET',
-	url: 'http://imac.local:3000/iterations',
-	beforeSend: function(xhr){
-		xhr.setRequestHeader('api_token', api_token);
-	},
-	// datatype: 'json',
-	error: function (e) {
-		console.log('error', e);
-		console.log(e.responseText);
-		console.log(e.statusText);
-		console.log(e.status);
-	},
-	success: function (s) {
-		console.log('success', s);
-	}
-});
+// $.ajax({
+// 	type: 'GET',
+// 	url: 'http://gentle-forest-6759.herokuapp.com/iterations',
+// 	beforeSend: function(xhr){
+// 		xhr.setRequestHeader('api_token', api_token);
+// 	},
+// 	// datatype: 'json',
+// 	error: function (e) {
+// 		console.log('error', e);
+// 		console.log(e.responseText);
+// 		console.log(e.statusText);
+// 		console.log(e.status);
+// 	},
+// 	success: function (s) {
+// 		console.log('success', s);
+// 	}
+// });
 
-$.ajax({
-	type: 'GET',
-	url: 'http://imac.local:3000/done_iterations',
-	beforeSend: function(xhr){
-		xhr.setRequestHeader('api_token', api_token);
-	},
-	// datatype: 'json',
-	error: function (e) {
-		console.log('error', e);
-		console.log(e.responseText);
-		console.log(e.statusText);
-		console.log(e.status);
-	},
-	success: function (s) {
-		console.log('success', s);
-	}
-});
+// $.ajax({
+// 	type: 'GET',
+// 	url: 'http://gentle-forest-6759.herokuapp.com/done_iterations',
+// 	beforeSend: function(xhr){
+// 		xhr.setRequestHeader('api_token', api_token);
+// 	},
+// 	// datatype: 'json',
+// 	error: function (e) {
+// 		console.log('error', e);
+// 		console.log(e.responseText);
+// 		console.log(e.statusText);
+// 		console.log(e.status);
+// 	},
+// 	success: function (s) {
+// 		console.log('success', s);
+// 	}
+// });
 
-$.ajax({
-	type: 'GET',
-	url: 'http://imac.local:3000/current_iterations',
-	beforeSend: function(xhr){
-		xhr.setRequestHeader('api_token', api_token);
-	},
-	// datatype: 'json',
-	error: function (e) {
-		console.log('error', e);
-		console.log(e.responseText);
-		console.log(e.statusText);
-		console.log(e.status);
-	},
-	success: function (s) {
-		console.log('success', s);
-	}
-});
+// $.ajax({
+// 	type: 'GET',
+// 	url: 'http://gentle-forest-6759.herokuapp.com/current_iterations',
+// 	beforeSend: function(xhr){
+// 		xhr.setRequestHeader('api_token', api_token);
+// 	},
+// 	// datatype: 'json',
+// 	error: function (e) {
+// 		console.log('error', e);
+// 		console.log(e.responseText);
+// 		console.log(e.statusText);
+// 		console.log(e.status);
+// 	},
+// 	success: function (s) {
+// 		console.log('success', s);
+// 	}
+// });
